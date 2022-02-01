0dcbdce15058131423808624cabd94b25937983f
KwH Kim <kwonhyun.kim@gmail.com>
2022-02-01 23:56:26 +0900

    02 packages

---
+++ 02_Packages_ADD.py
@@ -0,0 +1,10 @@
+## conda more
+
+* https://conda.io/projects/conda/en/latest/user-guide/tasks/manage-environments.html
+* https://conda.io/projects/conda/en/latest/commands.html
+
+
+## conda env remove할 때 주의점 
+
+* [왜 folder가 사라지지 않는가?](https://stackoverflow.com/questions/63022181/conda-env-remove-n-envname-does-not-delete-the-actual-folder)
+    - VS code나 다른 곳에서 conda 환경을 사용하지 않아야 완전히 삭제할 수 있다
